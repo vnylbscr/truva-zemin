9fd0f12c8e8d0d4b5a412ff77dc5db54f682a0ce
Mertqpien <mertgenc8@gmail.com>
2021-10-17 21:16:02 +0300

    fixing deploy warn

--- components/gallery/card.js
+++ components/gallery/card.js
@@ -4,7 +4,7 @@ import classes from '../../styles/Gallery.module.scss';
 const GalleryCard = ({ imageUrl, headerTitle, cardMeta, description }) => {
    return (
       <Card color='green' fluid className={classes.cardContainer}>
-         <Image className={classes.imgMain} src={imageUrl} wrapped ui={false} />
+         <Image className={classes.imgMain} src={imageUrl} alt='truva_zemin' wrapped ui={false} />
          <Card.Content header={headerTitle}>
             <Card.Description>{description}</Card.Description>
          </Card.Content>
